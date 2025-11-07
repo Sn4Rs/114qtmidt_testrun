bca89daf879918864ee0f6003d9671c40c45f54a
Sn4Rs <wisbi1997@gmail.com>
2025-11-07 18:54:45 +0800

    show 2 windows now

--- testrun/main.cpp
+++ testrun/main.cpp
@@ -1,11 +1,16 @@
 #include "testrun.h"
-
+#include <QWindow>
 #include <QApplication>
+#include <QWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    testrun w;
+    testrun w,w2;
+    //align w,w2
+    w.move(100,100);
     w.show();
+    w2.move(1000,100);
+    w2.show();
     return a.exec();
 }
